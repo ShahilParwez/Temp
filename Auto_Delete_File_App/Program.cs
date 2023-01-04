dc6f6b5143b4d76484eda9d0cb7891e3749cf143
Shahil Parwez <shahilparwez02@gmail.com>
2023-01-04 17:10:37 +0530

    ABC

---
+++ Auto_Delete_File_App/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Auto_Delete_File_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create 2 Variabel for Folder Path and Time Line
+
+            var path = ConfigurationManager.AppSettings["path"];
+            var time_line = ConfigurationManager.AppSettings["time_line"];            
+            
+
+            string[] files = Directory.GetFiles(@path);
+
+            foreach(string file in files)
+            {   
+                FileInfo fi = new FileInfo(file);
+                DateTime Cr_date = fi.CreationTime;
+
+                if (Cr_date < DateTime.Now.AddMinutes(Convert.ToInt32(time_line)))
+                    fi.Delete();
+                Console.WriteLine($"File  Creation date: {Cr_date}");
+
+            }
+            Console.WriteLine($"Files Deleted older then {time_line} days..");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
